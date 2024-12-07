970ffebdc0dec9a44780e76f6ab931fbb583a902
ruban wgs <ruban@whyglobalservices.com>
2024-12-07 21:33:03 +0530

    12/7

--- src/app/features/profile/change-password/change-password.component.ts
+++ src/app/features/profile/change-password/change-password.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../shared/parts/header/header.component";
 import { SidebarComponent } from "../../../shared/parts/sidebar/sidebar.component";
 import { FooterComponent } from "../../../shared/parts/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent,CommonModule],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.css'
 })
